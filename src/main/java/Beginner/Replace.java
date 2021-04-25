725c5b4cec3ba0fb1dd756a5ddc415624d9989c6
Serdar <serdar2225@gmail.com>
2021-04-25 19:40:14 -0400

    String refresher

---
+++ src/main/java/Beginner/Replace.java
@@ -0,0 +1,13 @@
+package Beginner;
+
+public class Replace {
+    public static void main(String[] args) {
+        String str = "The weather is so nice today";
+
+        String newStr = str.replace(" ", "-");
+        System.out.println(newStr);
+
+        System.out.println(str.substring(4));
+        System.out.println(str.substring(4,11));
+    }
+}
