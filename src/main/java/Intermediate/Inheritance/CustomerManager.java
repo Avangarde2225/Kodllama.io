c085b7281e63ca26e6eb96721b04f494b091adae
Serdar <serdar2225@gmail.com>
2021-05-05 00:48:24 -0400

    adding multiple customer at once

--- src/main/java/Intermediate/Inheritance/CustomerManager.java
+++ src/main/java/Intermediate/Inheritance/CustomerManager.java
@@ -4,6 +4,14 @@ public class CustomerManager {
 
     public void add(Customer customer){
         System.out.println(customer.customerNumber + " kaydedildi!");
+
+    }
+    // bulk insert
+    public void addMultiple(Customer[] customers){
+        for(Customer customer : customers){
+            add(customer);
+        }
+
     }
 
 
--- src/main/java/Intermediate/Inheritance/Main.java
+++ src/main/java/Intermediate/Inheritance/Main.java
@@ -5,12 +5,15 @@ public class Main {
         IndividualCustomer engin = new IndividualCustomer();
         engin.customerNumber = "12345";
 
+
         CorporateCustomer corp = new CorporateCustomer();
         corp.customerNumber = "3456778";
 
         CustomerManager cus = new CustomerManager();
-        cus.add(engin);
-        cus.add(corp);
+//        cus.add(engin);
+//        cus.add(corp);
+        Customer [] customers = {engin, corp};
+        cus.addMultiple(customers);
 
     }
 }
